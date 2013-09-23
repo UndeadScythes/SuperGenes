3c1e48fe8d71f201dc8cc54aae861b3c6e049da5
UndeadScythes <daveyognaut2@gmail.com>
2013-09-23 17:25:25 +0100

    Add character encoding argument handler

--- src/main/java/com/undeadscythes/supergenes/service/Load.java
+++ src/main/java/com/undeadscythes/supergenes/service/Load.java
@@ -7,11 +7,17 @@ import com.undeadscythes.genebase.*;
 import com.undeadscythes.genebase.gedcom.*;
 import com.undeadscythes.supergenes.*;
 import com.undeadscythes.tipscript.*;
+import java.io.*;
 
 /**
+ * Load a {@link GEDCOM} from file and parse it into a {@link GeneBase}.
+ *
  * @author UndeadScythes
  */
 public class Load implements Service {
+    /**
+     * {@inheritDoc}
+     */
     public boolean run(final AuthentiCmd cmdHandler, final String[] args) {
         final SuperGenes program = (SuperGenes)cmdHandler;
         final TipScript out = program.getTipScript();
@@ -20,11 +26,18 @@ public class Load implements Service {
             return true;
         }
         try {
-            final GeneBase geneBase = new GeneBase(new GEDCOM(args[0] + ".ged"));
+            final GeneBase geneBase;
+            if (args.length == 2) {
+                geneBase = new GeneBase(new GEDCOM(args[0] + ".ged", args[1]));
+            } else {
+                geneBase = new GeneBase(new GEDCOM(args[0] + ".ged"));
+            }
             program.addGeneBase(geneBase.getUID().toString(), geneBase);
             out.println("Loaded GEDCOM " + geneBase.getUID() + " from " + args[0] + ".ged.");
         } catch (ParsingException ex) {
             out.println("Cannot parse this file.");
+        } catch (UnsupportedEncodingException ex) {
+            out.println("Cannot use specified encoding.");
         }
         return true;
     }
