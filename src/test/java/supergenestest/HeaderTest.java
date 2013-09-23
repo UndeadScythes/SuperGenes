a823adfc69955fbddf1fa692a7ec259bfcc2b60c
UndeadScythes <daveyognaut2@gmail.com>
2013-09-23 17:27:33 +0100

    Add custom header test

---
+++ src/test/java/supergenestest/HeaderTest.java
@@ -0,0 +1,19 @@
+package supergenestest;
+
+import com.undeadscythes.genebase.record.*;
+import com.undeadscythes.supergenes.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import supergenestest.implementation.*;
+
+/**
+ * @author UndeadScythes
+ */
+public class HeaderTest {
+    @Test
+    public void testCustomHeader() {
+        assertEquals("getName", "SuperGenes", SuperGenesHeader.getStandard().getFirstFromPath("SOUR.NAME").getValue());
+        final Header header = new SuperGenesHeader(new SuperTGC55C().getGeneBase("TGC55C"));
+        assertEquals("getName", "GEDitCOM", header.getFirstFromPath("SOUR.DATA").getValue());
+    }
+}
