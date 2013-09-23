ffa0e93e88fade2aacffd467b6004bd64e4ac9b4
UndeadScythes <daveyognaut2@gmail.com>
2013-09-23 17:27:17 +0100

    Specify TipRedirect and character encoding

--- src/test/java/supergenestest/implementation/SuperTGC55C.java
+++ src/test/java/supergenestest/implementation/SuperTGC55C.java
@@ -2,12 +2,16 @@ package supergenestest.implementation;
 
 import com.undeadscythes.supergenes.*;
 import com.undeadscythes.supergenes.service.*;
+import com.undeadscythes.tipscript.*;
 
 /**
  * @author UndeadScythes
  */
-public final class SuperGenesImpl extends SuperGenes {
-    public SuperGenesImpl() {
-        new Load().run(this, new String[]{"src/test/resources/test"});
+public final class SuperTGC55C extends SuperGenes {
+    public static final TipRedirect REDIRECT = new TipRedirect();
+
+    public SuperTGC55C() {
+        super(System.in, REDIRECT);
+        new Load().run(this, new String[]{"src/test/resources/TGC55C", "US-ASCII"});
     }
 }
