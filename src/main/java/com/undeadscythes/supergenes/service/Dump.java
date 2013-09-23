8aefc7118e2efc50077cc28c99cdeadf2dfc1f29
UndeadScythes <daveyognaut2@gmail.com>
2013-09-23 17:24:56 +0100

    Change dump output file name

--- src/main/java/com/undeadscythes/supergenes/service/Dump.java
+++ src/main/java/com/undeadscythes/supergenes/service/Dump.java
@@ -3,6 +3,9 @@ package com.undeadscythes.supergenes.service;
 import com.undeadscythes.genebase.record.*;
 
 /**
+ * Dump the contents of the given {@link com.undeadscythes.genebase.GeneBase}
+ * to file along with various debugging information.
+ *
  * @author UndeadScythes
  */
 public class Dump extends AncestryService {
@@ -10,13 +13,13 @@ public class Dump extends AncestryService {
     public boolean run(final String[] args) {
         final Individual indi = getIndividual(args);
         if (indi.getUID().isNull()) {
-            if (!out.openFile(geneBase.getUID() + ".bug")) {
+            if (!out.openFile(geneBase.getUID() + ".dump")) {
                 out.println("Cannot open file.");
                 return true;
             }
             geneBase.dump(out);
             out.closeFile();
-            out.println("Output bug dump to " + geneBase.getUID() + ".bug.");
+            out.println("Output bug dump to " + geneBase.getUID() + ".dump.");
             return true;
         }
         indi.dump(out, "- ");
